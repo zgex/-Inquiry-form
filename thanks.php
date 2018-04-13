ee01917e7efe4176040c14578b5207800a6a9ceb
zgex <zgexgiex@gmail.com>
2018-04-13 22:00:22 +0900

    k

---
+++ thanks.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="UTF-8">
+<title>お問い合わせフォーム</title>
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+<div><h1>Company Name</h1></div>
+<div><h2>お問い合わせ</h2> </div>
+<div>
+        <div>
+		<h1>お問い合わせ 送信完了</h1>
+		<p>
+		お問い合わせありがとうございました。<br>
+		内容を確認のうえ、回答させて頂きます。<br>
+		しばらくお待ちください。
+		</p>
+		<a href="input.php">
+			<button type="button">お問い合わせに戻る</button>
+		</a>
+	</div>
+</div>
+</body>
+</html>
